999453997e742a9e5df022695b18865ad3788caa
Danielle Strickland <strickl6@illinois.edu>
2021-02-06 03:07:47 -0600

    added operator functions to js and calc layout to html

---
+++ calculator.js
@@ -0,0 +1,11 @@
+const add= (num1, num2)=>{return num1+num2};
+const subtract= (num1, num2)=>{return num1-num2};
+const multiply= (num1, num2)=>{return num1*num2};
+const divide= (num1, num2)=> {if(num2===0){
+  return "Can't divide by zero"
+}else{
+  return num1/num2;
+}};
+
+let display =document.getElementById("display");
+let buttons =document.getQuerySelector("button").onclick()
